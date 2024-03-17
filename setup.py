84ed18b3ae97258f8d910a565d3f0376bc3905f3
Midora Shiu <midorashiu@gmail.com>
2024-03-16 17:42:15 -0700

    update setup and pyproject for ver 4.2

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
     name="ubcaerodesign",  # How you named your package folder (MyLib)
     packages=["ubcaerodesign"],  # Chose the same as "name"
-    version="0.4.1",  # Start with a small number and increase it with every change you make
+    version="0.4.2",  # Start with a small number and increase it with every change you make
     license="GNU Affero General Public License v3.0",  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description="PyPi package for networking.py and loggingconfig",  # Give a short description about your library
     author="Midora",  # Type in your name
     author_email="midorashiu@gmail.com",  # Type in your E-Mail
     url="https://github.com/ubcaerodesign/ubcaerodesign",  # Provide either the link to your github or to your website
-    download_url="https://github.com/ubcaerodesign/ubcaerodesign/archive/refs/tags/pypi-0.4.1.tar.gz",  # I explain this later on
+    download_url="https://github.com/ubcaerodesign/ubcaerodesign/archive/refs/tags/pypi-0.4.2.tar.gz",  # I explain this later on
     keywords=["aerodesign"],  # Keywords that define your package best
     install_requires=["zmq"],  # I get to this in a second
     classifiers=[
